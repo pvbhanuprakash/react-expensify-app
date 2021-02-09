642a6042947c2e5192b37bbc97f4a0c78d330f4e
vivekreddy2 <padigelavivekreddy@gmail.com>
2021-02-09 21:58:44 +0530

    Added Material UI to Header.js (#1)

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,12 +1,77 @@
-import { NavLink } from "react-router-dom";
+import { makeStyles } from '@material-ui/core/styles';
+import { NavLink } from 'react-router-dom';
+import { AppBar, List, ListItem, ListItemText } from '@material-ui/core';
+import Toolbar from '@material-ui/core/Toolbar';
+import Typography from '@material-ui/core/Typography';
+import IconButton from '@material-ui/core/IconButton';
+import MenuIcon from '@material-ui/icons/Menu';
 
-const HeaderComponent = () => (
-    <div>
-      <h1>Expensify</h1>
-      <NavLink to="/" activeClassName="is-active" exact={true}>Dashboard</NavLink>
-      <NavLink to="/create" activeClassName="is-active">Create Expense</NavLink>
-      <NavLink to="/help" activeClassName="is-active">Help</NavLink>
-    </div>
-  );
+const useStyles = makeStyles((theme) => ({
+	root: {
+		flexGrow: 1,
+	},
+	menuButton: {
+		marginRight: theme.spacing(2),
+	},
+	title: {
+		flexGrow: 1,
+	},
+	navDisplayFlex: {
+		display: `flex`,
+		justifyContent: `space-between`,
+	},
+	linkText: {
+		textDecoration: `none`,
+		textTransform: `uppercase`,
+		color: `white`,
+	},
+}));
+
+const HeaderComponent = () => {
+	const classes = useStyles();
+	const navLinks = [
+		{ title: `Dashboard`, path: `/` },
+		{ title: `Create Expense`, path: `/create` },
+		{ title: `Edit Expense`, path: `/edit` },
+		{ title: `Help`, path: `/help` },
+	];
+	return (
+		<div>
+			<AppBar position='static'>
+				<Toolbar>
+					<IconButton
+						edge='start'
+						className={classes.menuButton}
+						color='inherit'
+						aria-label='menu'
+					>
+						<MenuIcon />
+					</IconButton>
+					<Typography variant='h6' className={classes.title}>
+						Expensify
+					</Typography>
+					<List
+						component='nav'
+						aria-labelledby='main navigation'
+						className={classes.navDisplayFlex}
+					>
+						{navLinks.map(({ title, path }) => (
+							<NavLink
+								to={path}
+								activeClassName='is-active'
+								exact
+								className={classes.linkText}
+							>
+								<ListItem button activeClassName='is-active'>
+									<ListItemText primary={title} activeClassName='is-active' />
+								</ListItem>
+							</NavLink>
+						))}
+					</List>
+				</Toolbar>
+			</AppBar>
+		</div>
+	);
+};
 
 export default HeaderComponent;
